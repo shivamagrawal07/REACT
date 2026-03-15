9f8957370e422c284e34b3a3c257a3c3fa095a6a
shivamagrawal07 <shivamagrawal041@gmail.com>
2026-03-16 00:52:15 +0530

    react

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,8 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+function MyApp(){
+  return(
+    <div>
+      <h1>Custom App !</h1>
+    </div>
+  )
+}
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <MyApp />
   </StrictMode>,
 )
